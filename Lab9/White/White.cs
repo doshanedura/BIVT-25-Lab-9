34b4d518e7154759250bff9bebd0aba23c1bb865
doshanedura <149386321+doshanedura@users.noreply.github.com>
2026-04-19 15:47:10 +0300

    Update White.cs

--- Lab9/White/White.cs
+++ Lab9/White/White.cs
@@ -1,3 +1,43 @@
-﻿namespace Lab9.White
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Lab9.White
 {
+    public abstract class White
+    {
+        private string _input;
+        private object? _output;
+
+        public string Input => _input;
+        public object? Output => _output;
+
+        protected White(string text)
+        {
+            _input = text;
+            _output = GetDefaultOutput();
+        }
+
+        protected virtual object? GetDefaultOutput()
+        {
+            return null;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+
+        protected void SetOutput(object value)
+        {
+            _output = value;
+        }
+
+        public override abstract string ToString();
+    }
 }
